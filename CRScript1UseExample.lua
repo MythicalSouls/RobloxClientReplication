576928aa2a3726dae59964d06425c1e648ecdf59
MythicalSouls <dyzzang2012@gmail.com>
2025-05-18 19:40:02 +0900

    Create CRScript1UseExample.lua

---
+++ CRScript1UseExample.lua
@@ -0,0 +1,2 @@
+_G.TARGET_NAME = game.Players.LocalPlayer.Name
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MythicalSouls/RobloxClientReplication/refs/heads/main/ClientReplicationScript1FixAttermpt1.lua"))();
